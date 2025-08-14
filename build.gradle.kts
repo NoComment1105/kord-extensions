af441972bd63758db5ef2058a15633a88a3a015d
Gareth Coles <gareth@gareth-coles.dev>
2025-08-14 12:39:17 +0100

    SQ: Provide full path

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ sonar {
 		property("sonar.host.url", System.getenv()["SONAR_HOST_URL"] ?: "")
 		property("sonar.token", System.getenv()["SONAR_TOKEN"] ?: "" )
 		property("sonar.scm.provider", "git")
-		property("sonar.coverage.jacoco.xmlReportPaths", "build/reports/kover/")
+		property("sonar.coverage.jacoco.xmlReportPaths", "build/reports/kover/report.xml")
 
 		property("sonar.links.homepage", homepageUrl)
 		property("sonar.links.ci", "$gitUrl/actions")
